99ab8c5d2de9462b8ece02a42c62b050315210fa
Ben Firth <bfirth93@gmail.com>
2020-10-26 13:05:11 -0600

    Add climber container

---
+++ src/components/ClimberContainer/ClimberContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ClimberContainer = () => {
+
+  return (
+    <section>
+      <h1>ClimberContainer!</h1>
+    </section>
+  )
+}
+
+export default ClimberContainer;
