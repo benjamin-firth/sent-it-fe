8c4e6947ef201c486eede09c5bf44a68114ce350
Ben Firth <bfirth93@gmail.com>
2020-10-28 12:54:16 -0600

    Add lowermain background

--- src/components/LowerMain/LowerMain.js
+++ src/components/LowerMain/LowerMain.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import ClimbContainer from '../ClimbContainer/ClimbContainer';
 import ClimbForm from '../ClimbForm/ClimbForm';
+import bouldererPic from '../../images/bouldererpic.jpg';
 
 const LowerMain = () => {
   const [showAddForm, setShowAddForm] = useState(false);
@@ -11,19 +12,34 @@ const LowerMain = () => {
   };
 
   return (
-    <section>
-      {showAddForm ?
-        <>
-          <button type='button' onClick={handleClick}>Hide Form</button>
-          <ClimbForm />
-        </> :
-        <>
-          <button type='button' onClick={handleClick}>Add Climb</button>
-          <ClimbContainer />
-        </>
-      }   
-    </section>
+    <StyledLowerBackground>
+      <StyledLowerMain>
+        {showAddForm ?
+          <>
+            <button type='button' onClick={handleClick}>Hide Form</button>
+            <ClimbForm />
+          </> :
+          <>
+            <button type='button' onClick={handleClick}>Add Climb</button>
+            <ClimbContainer />
+          </>
+        }   
+      </StyledLowerMain>
+    </StyledLowerBackground>
   );
 };
+
+const StyledLowerBackground = styled.div`
+  background: url(${bouldererPic}) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+`;
+
+const StyledLowerMain = styled.section`
+  height: 88vh;
+  background-color: rgba(202, 209, 238,0.6);
+`;
 
 export default LowerMain;
