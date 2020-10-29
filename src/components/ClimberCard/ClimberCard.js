db5e9fcb30b8628aec591aa4528fc7e488798e2c
Ben Firth <bfirth93@gmail.com>
2020-10-29 13:16:22 -0600

    Add profile card skeleton

--- src/components/ClimberCard/ClimberCard.js
+++ src/components/ClimberCard/ClimberCard.js
@@ -1,12 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+import profilePic from '../../images/no-profile-img.png';
 
 const ClimberCard = () => {
 
   return (
-    <section>
-      <h1>ClimberCard!</h1>
-    </section>
+    <StyledClimberCard>
+      <StyledClimberCardTitle>
+        <StyledProfileImageContainer>
+          <img src={profilePic} alt='Profile picture'></img>
+        </StyledProfileImageContainer>
+        <h3>Whitney Burton</h3>
+      </StyledClimberCardTitle>
+      <p>Location: Golden</p>
+      <p>Total Climbs: 40</p>
+      <p>Recent Sends: 3 </p>
+    </StyledClimberCard>
   )
 }
+
+const StyledClimberCard = styled.article`
+  background-color: #101628;
+  border: 1px solid #000;
+  border-radius: 5px;
+  color: #FFF;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  padding: 15px;
+  width: 220px;
+`;
+
+const StyledClimberCardTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const StyledProfileImageContainer = styled.section`
+  border: 1px solid #CAD1EE;
+  border-radius: 10px;
+  height: 8vh;
+
+  img {
+    height: 100%;
+  }
+`;
 
 export default ClimberCard;
