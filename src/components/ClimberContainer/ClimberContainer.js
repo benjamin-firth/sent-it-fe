59eb0c9cb093cdb3b2113ccf686fb081b2348871
Ben Firth <bfirth93@gmail.com>
2020-11-03 12:23:34 -0700

    Add climber container basic styling

--- src/components/ClimberContainer/ClimberContainer.js
+++ src/components/ClimberContainer/ClimberContainer.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import ClimberCard from '../ClimberCard/ClimberCard';
 
 const ClimberContainer = () => {
 
   return (
-    <section>
-      <h1>ClimberContainer!</h1>
-    </section>
-  )
-}
+    <StyledClimberMain>
+      <h2>Climbers Near You</h2>
+      <StyledClimberWrapper>
+        <ClimberCard />
+        <ClimberCard />
+        <ClimberCard />
+        <ClimberCard />
+        <ClimberCard />
+        <ClimberCard />
+      </StyledClimberWrapper>
+    </StyledClimberMain>
+  );
+};
+
+const StyledClimberMain = styled.div`
+  h2 {
+    font-size: 1.7em;
+  }
+`;
+
+const StyledClimberWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 
 export default ClimberContainer;
